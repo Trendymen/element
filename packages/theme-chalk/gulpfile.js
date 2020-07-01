adc095175ca09f6864a797151f67152596f825c6
Trendymen <476212964@qq.com>
2020-07-01 17:31:52 +0800

    modify gulpfile.js to remove sourcemap

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -7,17 +7,14 @@ const cssmin = require('gulp-cssmin');
 const cssnano = require("cssnano")
 const autoprefixer = require("autoprefixer");
 const postcss = require("gulp-postcss");
-const sourcemaps = require('gulp-sourcemaps')
 
 function compile() {
   return src('./src/*.scss')
     .pipe(sass.sync())
-    .pipe(sourcemaps.init())
     .pipe(postcss([
       autoprefixer({overrideBrowserslist: ['ie > 9', 'last 2 versions']}),
       cssnano()
     ]))
-    .pipe( sourcemaps.write('.') )
     .pipe(dest('./lib'));
 }
 
